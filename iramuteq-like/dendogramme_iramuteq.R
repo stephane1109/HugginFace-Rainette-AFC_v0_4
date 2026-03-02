f99061b98b2a41498aaeb78e3e5c03daf907c5da
Stéphane Meurisse <141574089+stephane1109@users.noreply.github.com>
2026-03-02 19:57:43 +0100

    Fix affichage du dendrogramme IRaMuTeQ-like

--- iramuteq-like/dendogramme_iramuteq.R
+++ iramuteq-like/dendogramme_iramuteq.R
@@ -3,16 +3,37 @@
 tracer_dendogramme_iramuteq_ui <- function(rv,
                                             top_n_terms = 4,
                                             orientation = "vertical") {
-  if (is.null(rv$res) || is.null(rv$res$chd)) {
+  if (is.null(rv$res) && is.null(rv$res_chd)) {
     plot.new()
     text(0.5, 0.5, "Dendrogramme CHD indisponible.", cex = 1.1)
     return(invisible(NULL))
   }
 
+  chd_obj <- NULL
+  if (!is.null(rv$res$chd)) {
+    chd_obj <- rv$res$chd
+  } else if (!is.null(rv$res_chd)) {
+    chd_obj <- rv$res_chd
+  } else if (exists("obtenir_objet_dendrogramme", mode = "function", inherits = TRUE)) {
+    chd_obj <- obtenir_objet_dendrogramme(rv$res)
+  }
+
+  terminales <- if (!is.null(rv$res$terminales)) rv$res$terminales else NULL
+  classes <- if (!is.null(rv$res$classes)) rv$res$classes else NULL
+  if (is.null(classes) && !is.null(rv$filtered_corpus) && "Classes" %in% names(docvars(rv$filtered_corpus))) {
+    classes <- docvars(rv$filtered_corpus)$Classes
+  }
+
+  if (is.null(chd_obj)) {
+    plot.new()
+    text(0.5, 0.5, "Dendrogramme CHD indisponible (objet CHD introuvable).", cex = 1.1)
+    return(invisible(NULL))
+  }
+
   tracer_dendrogramme_chd_iramuteq(
-    chd_obj = rv$res$chd,
-    terminales = rv$res$terminales,
-    classes = rv$res$classes,
+    chd_obj = chd_obj,
+    terminales = terminales,
+    classes = classes,
     res_stats_df = rv$res_stats_df,
     top_n_terms = top_n_terms,
     orientation = orientation
